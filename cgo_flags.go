4d67cb2d76c14b7d117fbcd95fa9586b7b92e048
Raphael 'kena' Poss <knz@thaumogen.net>
2015-12-29 14:28:17 +0100

    Pass flags through for DragonflyBSD.

--- cgo_flags.go
+++ cgo_flags.go
@@ -16,10 +16,12 @@ import (
 // #cgo darwin CPPFLAGS: -DOS_MACOSX
 // #cgo linux CPPFLAGS: -DOS_LINUX
 // #cgo freebsd CPPFLAGS: -DOS_FREEBSD
+// #cgo dragonfly CPPFLAGS: -DOS_DRAGONFLY
 // #cgo CXXFLAGS: -W -Wextra -Wall -Wsign-compare -Wshadow -Wno-unused-parameter
 // #cgo CXXFLAGS: -std=c++11 -fno-omit-frame-pointer -momit-leaf-frame-pointer
 // #cgo darwin CXXFLAGS: -Wshorten-64-to-32
 // #cgo freebsd CXXFLAGS: -Wshorten-64-to-32
+// #cgo dragonfly CXXFLAGS: -Wshorten-64-to-32
 // #cgo darwin LDFLAGS: -Wl,-undefined -Wl,dynamic_lookup
 // #cgo !darwin LDFLAGS: -Wl,-unresolved-symbols=ignore-all
 import "C"
