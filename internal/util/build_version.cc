ae2186e099d7e900153a463cb429710a3af30cb9
Peter Mattis <petermattis@gmail.com>
2017-01-19 10:17:02 -0500

    Update upstream to 5.0.2.

--- internal/util/build_version.cc
+++ internal/util/build_version.cc
@@ -1,4 +1,4 @@
 #include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:894d369c4ea259b65f75563feeb06bea322a8450";
-const char* rocksdb_build_git_date = "rocksdb_build_git_date:2017-01-12";
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:b49ad714096d772eddb5f543274fbe5c37bdafd0";
+const char* rocksdb_build_git_date = "rocksdb_build_git_date:2017-01-19";
 const char* rocksdb_build_compile_date = __DATE__;
